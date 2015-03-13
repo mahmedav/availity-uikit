f28bdc9801cd6a4d9cd49fa5d3d2382fd30121ef
Robert Ventrone <rjventrone@gmail.com>
2015-03-13 14:11:33 -0400

    updated publish with gh-pages

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var spawn = require('child_process').spawn;
 var bower = require('../../bower.json');
+var gh_pages = require('gulp-gh-pages');
 
 gulp.task('publish', ['publish:npm', 'publish:bower']);
 
@@ -12,4 +13,11 @@ gulp.task('publish:npm', function(done) {
 
 gulp.task('publish:bower', function(done) {
   spawn('bower', ['register', bower.name, bower.repository], { stdio: 'inherit' }).on('close', done);
+});
+
+gulp.task('publish:ghpages', ['build:ghpages'], function () {
+  var options = {};
+  options.cacheDir = 'tmp/gh-pages'; //Check gulp-gh-pages to see if they have fixed the issue with tmp directory
+  return gulp.src('./build/guide/**/*')
+         .pipe(gh_pages(options));
 });
